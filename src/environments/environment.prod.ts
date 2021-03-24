22f93758abe21ff2c44dddaf1fbac6170b55d5b0
Thieles Martins <thielesmartins@MacBook-Air-de-Thieles.local>
2021-03-24 01:38:54 -0300

    ajuste environment

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,4 @@
 export const environment = {
   production: true,
-  apiUrl: 'https://app-cadastro-pessoa-back.herokuapp.com/pessoa',
+  apiUrl: 'https://app-cadastro-pessoa-back.herokuapp.com/pessoa'
 };
