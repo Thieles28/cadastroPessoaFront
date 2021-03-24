4e14ba59704df5b1d6aebaa0989262655a39dd60
Thieles Martins <thielesmartins@MacBook-Air-de-Thieles.local>
2021-03-24 01:04:31 -0300

    ajuste pacote

--- src/app/layouts/auth-layout/auth-layout.module.ts
+++ src/app/layouts/auth-layout/auth-layout.module.ts
@@ -4,42 +4,16 @@ import { CommonModule } from '@angular/common';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { AuthLayoutRoutes } from './auth-layout.routing';
 import { LoginComponent } from '../../pages/login/login.component';
-import {
-  SocialLoginModule,
-  AuthServiceConfig,
-  GoogleLoginProvider,
-  FacebookLoginProvider,
-} from "angular5-social-login";
 import { BrowserModule } from '@angular/platform-browser';
 
-// Configs
-export function getAuthServiceConfigs() {
-  let config = new AuthServiceConfig(
-      [
-        {
-          id: GoogleLoginProvider.PROVIDER_ID,
-          provider: new GoogleLoginProvider("795641066279-rcq9hihqdo00ubh5guhqeds092m1kuuf.apps.googleusercontent.com")
-        },
-      ]
-  );
-  return config;
-}
 @NgModule({
   imports: [
     CommonModule,
     RouterModule.forChild(AuthLayoutRoutes),
-    FormsModule,
-    BrowserModule,
-    SocialLoginModule
+    FormsModule
   ],
   declarations: [
     LoginComponent
-  ],
-  providers: [
-    {
-      provide: AuthServiceConfig,
-      useFactory: getAuthServiceConfigs
-    }
-  ],
+  ]
 })
 export class AuthLayoutModule { }
--- src/app/pages/login/login.component.ts
+++ src/app/pages/login/login.component.ts
@@ -1,41 +1,14 @@
 import { Component, OnInit, OnDestroy } from '@angular/core';
-import { AuthService, FacebookLoginProvider, GoogleLoginProvider, SocialUser } from 'angular5-social-login';
 @Component({
   selector: 'app-login',
   templateUrl: './login.component.html',
   styleUrls: ['./login.component.scss']
 })
 export class LoginComponent implements OnInit, OnDestroy {
-  private user: SocialUser;
-  public authorized: boolean = false;
 
-  constructor(private socialAuthService: AuthService) {}
+  constructor() {}
 
   ngOnInit() {
-  }
-
-  public socialSignIn(socialPlatform : string) {
-
-    let socialPlatformProvider;
-    if(socialPlatform == "google"){
-      socialPlatformProvider = GoogleLoginProvider.PROVIDER_ID;
-    }
-
-    this.socialAuthService.signIn(socialPlatformProvider).then(
-      (userData) => {
-        console.log(socialPlatform+" sign in data : " , userData);
-
-        if (userData != null) {
-               this.authorized = true;
-               this.user = userData;
-            }
-      }
-    );
-  }
-
-  public signOut(){
-      this.socialAuthService.signOut();
-      this.authorized = false;
   }
 
   ngOnDestroy() {
